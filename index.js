9960cb92058cae953cb67478700409c8128470aa
nandasino <fernandamsino@gmail.com>
2022-11-11 12:08:50 -0300

    formato de mensagem e participante

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
-app.listen(500, ()=>{
+try{
+    await mongoClient.connect();
+    db = mongoClient.db("batepapoUol");
+} catch (err) {
+    console.log(err);
+};
+
+const participantShema = joi.object({
+    name: joi.string().required().empty(),
+    lastStatus: joi.number()
+});
+
+const messageShema= joi.object({
+    from: joi.string().required().empty(),
+    to: joi.string().required().empty(),
+    text: joi.string().required().empty(),
+    type: joi.string().required().valid("message", "private_message"),
+    time: joi.string().required().empty(),
+});
+
+app.listen(5000, ()=>{
     console.log("Server running in port 5000")
 });
